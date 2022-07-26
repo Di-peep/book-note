024b0b4a5d7a69a02ad72c3e2c948de9a28e280f
Dmytro <vdude112358@gmail.com>
2022-07-26 12:27:50 +0300

    Create objects db and migrate

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+import config
 
 
 app = Flask(__name__)
+app.config.from_object(config.Config)
+
 api = Api(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from src import routes
