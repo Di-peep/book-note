7e5e674c13e121961b52c86b8c1b35d66fabc378
Dmytro <vdude112358@gmail.com>
2022-07-28 10:42:56 +0300

    Add swagger config

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 import config
 
@@ -13,5 +14,17 @@ api = Api(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+SWAGGER_URL = "/swagger"
+API_URL = "/static/swagger.json"
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        "app_name": "Book note"
+    }
+)
+
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 from src import routes, models
