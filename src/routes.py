a30d4f2008c0b58feb2bb14c62351c6620454d59
Dmytro <vdude112358@gmail.com>
2022-08-01 22:09:34 +0300

    Fix route Authors

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,8 @@
 from src import api
+from src.resources.authors import Authors
 from src.resources.books import Books
 from src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke', '/smoke/', strict_slashes=False)
 api.add_resource(Books, '/books', '/books/<uuid>', strict_slashes=False)
-api.add_resource(Books, '/authors', '/authors/<uuid>', strict_slashes=False)
+api.add_resource(Authors, '/authors', '/authors/<uuid>', strict_slashes=False)
