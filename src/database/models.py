737b25ad1884c654419fa414ba6a73819dcd9aa6
Dmytro <vdude112358@gmail.com>
2022-08-02 23:08:40 +0300

    Add relationship one-to-many

--- src/database/models.py
+++ src/database/models.py
@@ -4,12 +4,14 @@ from src import db
 
 
 class Book(db.Model):
-    __tablename__ = "books"
+    __tablename__ = "book"
 
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(36), unique=True)
     title = db.Column(db.String(120), nullable=False)
-    author = db.Column(db.String(80), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
+    author = db.relationship("Author", back_populates="books")
+
     description = db.Column(db.Text, unique=True, nullable=False)
     release_date = db.Column(db.Date)
     publisher = db.Column(db.String(60))
@@ -29,31 +31,24 @@ class Book(db.Model):
     def __repr__(self):
         return f'Book({self.uuid}, {self.title}, {self.author})'
 
-    def to_dict(self):
-        return {
-            "uuid": self.uuid,
-            "title": self.title,
-            "author": self.author,
-            "description": self.description,
-            "release_date": self.release_date.strftime("%Y"),
-            "publisher": self.publisher,
-            "language": self.language,
-            "rating": self.rating
-        }
-
 
 class Author(db.Model):
-    __tablename__ = "authors"
+    __tablename__ = "author"
 
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(36), unique=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.Text, unique=True, nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    books = db.relationship("Book", back_populates="author")
 
-    def __init__(self, name, description):
+    def __init__(self, name, description="", books=None):
         self.uuid = str(uuid.uuid4())
         self.name = name
         self.description = description
+        if not books:
+            self.books = []
+        else:
+            self.books = books
 
     def __repr__(self):
         return f"Author({self.uuid}, {self.name})"
