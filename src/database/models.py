945fb2cdb87d2707818de6960d1521f0733324fc
Dmytro <vdude112358@gmail.com>
2022-08-03 22:11:35 +0300

    Create User model

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,7 @@
 import uuid
 
+from werkzeug.security import generate_password_hash
+
 from src import db
 
 
@@ -52,3 +54,24 @@ class Author(db.Model):
 
     def __repr__(self):
         return f"Author({self.uuid}, {self.name})"
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(36), unique=True)
+    username = db.Column(db.String(48), unique=True, nullable=False)
+    email = db.Column(db.String(54), unique=True, nullable=False)
+    password = db.Column(db.String(254), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, email, password, is_admin=False):
+        self.uuid = str(uuid.uuid4())
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.is_admin = is_admin
+
+    def __repr__(self):
+        return f"User({self.name}, {self.email}, {self.uuid})"
