06b23c017cf76a4506d4b755fc742017b8c66f59
Dmytro <vdude112358@gmail.com>
2022-08-05 23:05:49 +0300

    Add routes /register and /login

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
 from src import api
+from src.resources.auth import RegisterUser, LoginUser
 from src.resources.authors import Authors
 from src.resources.books import Books
 from src.resources.smoke import Smoke
@@ -6,3 +7,6 @@ from src.resources.smoke import Smoke
 api.add_resource(Smoke, '/smoke', '/smoke/', strict_slashes=False)
 api.add_resource(Books, '/books', '/books/<uuid>', strict_slashes=False)
 api.add_resource(Authors, '/authors', '/authors/<uuid>', strict_slashes=False)
+
+api.add_resource(RegisterUser, '/register', strict_slashes=False)
+api.add_resource(LoginUser, '/login', strict_slashes=False)
